265ca65c55dca55d8736086bdab092b5f0d5fa26
MohamadKh75 <khajavi_mohamad@yahoo.com>
2020-03-02 19:09:35 +0330

    Cleanups

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Engine: Hermes</Text>
+  </View>
+);
+
+export default App;
