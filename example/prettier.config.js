c44d3bec070a872c3c2a69bc907a5fcd0ca25623
MohamadKh75 <khajavi_mohamad@yahoo.com>
2020-03-02 19:09:17 +0330

    Update Prettier rules

---
+++ example/prettier.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'consistent',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+  vueIndentScriptAndStyle: false
+};
